997b724c2376b4de7a790b713dba18ce2502a2a0
OmBarabhai <oraghuwanshi37@gmail.com>
2025-02-03 08:02:21 +0000

    dataTypes

---
+++ 01.Basics/02.dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+console.log(3
+    +3) // CODE READABILITY SHOULD BE HIGH
+    console.log("Hitesh")
+
+let name ="hitesh"
+let age=18
+let isLoggedIn= false
+
+let state=null
+ 
+
+//number =>2 to power 54
+//bigint
+//string=>""
+//boolean=> true or false
+//null => standalone value
+//undefined =>
+//symbol=> unique
+
+//Object
+
+console.log(typeof "om");
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object

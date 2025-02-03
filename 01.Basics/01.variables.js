85b571987450102a97e31dfbff72d62735b90f30
OmBarabhai <oraghuwanshi37@gmail.com>
2025-02-03 08:02:09 +0000

    variables

---
+++ 01.Basics/01.variables.js
@@ -0,0 +1,22 @@
+const accId= 1343
+let accpwd="12344"
+
+var acccemail="om@gmail.com"
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+ 
+
+accCity="amt"
+let accstate;
+
+// accId=2 //Not allowed
+accemail="hc@hc.com"
+accpwd="21212121"
+accCity="Bengluru"
+
+console.log(accId);
+
+console.table([accId,accemail,accpwd,accCity,accstate])

cbd497906cb3d4ed7add111a7d3c863d7b19dc7d
OmBarabhai <oraghuwanshi37@gmail.com>
2025-02-03 11:12:15 +0000

    conversionOperation

---
+++ 01.Basics/03.conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "om"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNum = Number(score)
+
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+//"33" => 33
+//"33abc" => NaN (Not a Number)
+//true => 1; false => 0
+
+let isLoggedIn= "om"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "om" => true
+
+let someNumber = 33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
